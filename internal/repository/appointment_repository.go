7f2692af65948bb9113f0f755e5e771762edd767
vinicius <viniciusdosanjos63@gmail.com>
2025-11-20 16:34:12 -0300

    test(service): appointment suggestion on create

--- internal/repository/appointment_repository.go
+++ internal/repository/appointment_repository.go
@@ -16,42 +16,42 @@ type AppointmentRepository interface {
 	ListAll() ([]models.Appointment, error)
 }
 
-type appointmentRepo struct {
+type sqlAppointmentRepo struct {
 	db *gorm.DB
 }
 
 func NewAppointmentRepository(db *gorm.DB) AppointmentRepository {
-	return &appointmentRepo{db}
+	return &sqlAppointmentRepo{db}
 }
 
-func (r *appointmentRepo) Create(ap models.Appointment) (models.Appointment, error) {
+func (r *sqlAppointmentRepo) Create(ap models.Appointment) (models.Appointment, error) {
 	err := r.db.Create(&ap).Error
 	return ap, err
 }
 
-func (r *appointmentRepo) Update(ap models.Appointment) error {
+func (r *sqlAppointmentRepo) Update(ap models.Appointment) error {
 	return r.db.Save(&ap).Error
 }
 
-func (r *appointmentRepo) FindByID(id uint) (models.Appointment, error) {
+func (r *sqlAppointmentRepo) FindByID(id uint) (models.Appointment, error) {
 	var ap models.Appointment
 	err := r.db.Preload("Customer").First(&ap, id).Error
 	return ap, err
 }
 
-func (r *appointmentRepo) FindCustomerAppointmentsInWeek(customerID uint, weekStart, weekEnd time.Time) ([]models.Appointment, error) {
+func (r *sqlAppointmentRepo) FindCustomerAppointmentsInWeek(customerID uint, weekStart, weekEnd time.Time) ([]models.Appointment, error) {
 	var list []models.Appointment
 	err := r.db.Where("customer_id = ? AND date BETWEEN ? AND ?", customerID, weekStart, weekEnd).Find(&list).Error
 	return list, err
 }
 
-func (r *appointmentRepo) ListByPeriod(start, end time.Time) ([]models.Appointment, error) {
+func (r *sqlAppointmentRepo) ListByPeriod(start, end time.Time) ([]models.Appointment, error) {
 	var list []models.Appointment
 	err := r.db.Where("date BETWEEN ? AND ?", start, end).Find(&list).Error
 	return list, err
 }
 
-func (r *appointmentRepo) ListAll() ([]models.Appointment, error) {
+func (r *sqlAppointmentRepo) ListAll() ([]models.Appointment, error) {
 	var list []models.Appointment
 	err := r.db.Find(&list).Error
 	return list, err
--- internal/service/appointments_service_test.go
+++ internal/service/appointments_service_test.go
@@ -2,10 +2,12 @@ package service
 
 import (
 	"testing"
+	"time"
 
 	"github.com/ViniciusBoroto/cabeleleila_leila/internal/mocks"
 	"github.com/ViniciusBoroto/cabeleleila_leila/internal/models"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateService_WithSuggestion(t *testing.T) {
@@ -13,7 +15,14 @@ func TestCreateService_WithSuggestion(t *testing.T) {
 	defer ctrl.Finish()
 	mockRepo := mocks.NewMockAppointmentRepository(ctrl)
 	apSrv := NewAppointmentService(mockRepo)
+	existentAp := models.Appointment{ID: 2, Date: time.Now().AddDate(0, 0, 2)}
 
 	mockRepo.EXPECT().Create(gomock.Any()).Return(models.Appointment{ID: 5}, nil)
+	mockRepo.EXPECT().FindCustomerAppointmentsInWeek(gomock.Any(), gomock.Any(), gomock.Any()).Return([]models.Appointment{existentAp}, nil)
 
+	ap, suggestion, err := apSrv.CreateAppointment(1, []models.Service{{ID: 1, Name: "Corte"}}, time.Now().AddDate(0, 0, 3))
+	assert.NoError(t, err)
+	assert.NotNil(t, suggestion)
+	assert.Equal(t, uint(5), ap.ID)
+	assert.Equal(t, existentAp.Date, *suggestion)
 }
